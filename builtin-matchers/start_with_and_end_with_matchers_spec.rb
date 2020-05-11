fb9d461068bb1449d139a8bbcab39986c799b884
RizaHKhan <khanriza@gmail.com>
2020-05-10 20:42:02 -0400

    added start with and end with matchers examples

---
+++ builtin-matchers/start_with_and_end_with_matchers_spec.rb
@@ -0,0 +1,23 @@
+RSpec.describe '::start with and end with matchers' do
+  describe 'caterpillar' do
+    it 'should check for substring at the beginning or end' do
+      expect(subject).to start_with('cat')
+      expect(subject).to end_with('pillar')
+    end
+
+    it { is_expected.to start_with('cat') }
+    it { is_expected.to end_with('pillar') }
+  end
+
+  describe [:a, :b, :c, :d] do
+    it 'should check for elements at beginning or end of the array' do
+      expect(subject).to start_with(:a)
+      expect(subject).to start_with(:a, :b)
+      expect(subject).to start_with(:a, :b, :c)
+      expect(subject).to end_with(:d)
+    end
+
+    it { is_expected.to start_with(:a) }
+    it { is_expected.to end_with(:d) }
+  end
+end
